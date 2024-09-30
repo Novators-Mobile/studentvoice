ab2c088efb88b92e93c7caea94f6b1f05dcda16e
Alexander <dayboval_official@mail.ru>
2024-10-01 01:00:00 +0500

    [add] Login page

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+
+import App from './App';
+
+import './styles/reset.css';
+import './styles/style.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
---
+++ src/pages/Login.tsx
@@ -0,0 +1,21 @@
+export default function Login() {
+  return (
+    <>
+      <div className="login">
+        <div className="login__wrap">
+          <div className="login__greeting-card">
+            <h1 className="login__title title">Добро пожаловать в&nbsp;StudentVoice!</h1>
+            <p className="login__text">Здесь тебя услышат</p>
+          </div>
+
+          <form action="" className="login__auth-form">
+            <input type="text" className="login__auth-form_field" placeholder="Логин" required />
+            <input type="password" className="login__auth-form_field" placeholder="Пароль" required />
+
+            <button type="submit" className="login__submit-btn">Войти</button>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
