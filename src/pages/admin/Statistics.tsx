a1271d0fc35b4a0fd4a21a99ffa19d0bbdd4e2c9
Alexander <dayboval_official@mail.ru>
2025-01-12 01:43:12 +0500

    [fix] Small stats by week fix

--- src/pages/admin/Statistics.tsx
+++ src/pages/admin/Statistics.tsx
@@ -3,10 +3,12 @@ import { getStatsByWeek, TStatsWeekItem } from "../../api/admin/statsApi";
 import { statsWeekToGraph } from "../../utils/adapter";
 import StatisticsGraph from "../../components/StatisticsGraph";
 import { useSearchParams } from "react-router-dom";
+import { getInstitute, TInstitute } from "../../api/admin/institutesApi";
 
 function Statistics() {
   const [stats, setStats] = useState<TStatsWeekItem[]>([]);
   const [searchParams] = useSearchParams();
+  const [institute, setInstitute] = useState<TInstitute>();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,6 +18,8 @@ function Statistics() {
           year: searchParams.get("year")!,
           month: searchParams.get("month")!,
         });
+        const instituteInfo = await getInstitute(searchParams.get("instituteId")!);
+        setInstitute(instituteInfo);
         setStats(statsData);
       } catch (err) {
         console.error("Ошибка при получении данных: ", err);
@@ -28,6 +32,12 @@ function Statistics() {
   return (
     <div className="statistics">
       <h1 className="statistics-header header-text">Статистика по неделям</h1>
+
+      <div className="stats-weeks__info">
+        <p><b>Институт:</b> {institute?.short_name}</p>
+        <p><b>Год:</b> {searchParams.get("year")}</p>
+        <p><b>Месяц:</b> {searchParams.get("month")}</p>
+      </div>
 
       <StatisticsGraph data={statsWeekToGraph(stats!)} />
     </div>
